2bc436e2ebe83e479280c9fa401efc6c61aac1b4
diego gomez <diegog.v465@gmail.com>
2024-10-02 14:39:31 -0300

    Integración de nuevos atributos (clase autor) y modificacion clase biblioteca

--- Ejer_Bliblioteca/clases/autor.py
+++ Ejer_Bliblioteca/clases/autor.py
@@ -1,6 +1,8 @@
 class Autor:
-    def __init__(self, id_autor, a_nombre, a_apellido, biografia):
+    def __init__(self, id_autor, a_nombre, a_apellido, biografia, nacionalidad, fech_nacimiento):
         self.id_autor = id_autor
         self.a_nombre = a_nombre
         self.a_apellido = a_apellido
         self.biografia = biografia
+        self.nacionalidad = nacionalidad
+        self.fech_nacimiento = fech_nacimiento
--- Ejer_Bliblioteca/clases/biblioteca.py
+++ Ejer_Bliblioteca/clases/biblioteca.py
@@ -1,5 +1,5 @@
 class Bliblioteca:
-    def __init__(self, b_nombre, direccion, b_telefono):
+    def __init__(self, b_nombre, b_direccion, b_telefono):
         self.b_nombre = b_nombre
-        self.direccion = direccion
+        self.direccion = b_direccion
         self.b_telefono = b_telefono
