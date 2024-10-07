dc434dc7339eb37889d1b5f6667feb208fae7982
diego gomez <diegog.v465@gmail.com>
2024-10-07 16:22:49 -0300

    comentario

--- Ejer_Bliblioteca/clases/biblioteca.py
+++ Ejer_Bliblioteca/clases/biblioteca.py
@@ -1,7 +1,8 @@
 from contacto import Contacto
 class Biblioteca(Contacto):
-    def __init__(self, id_biblio, b_nombre, b_direccion, id_contacto):
-        Contacto.__init__(self,id_contacto)
+    def __init__(self, id_biblio, b_nombre, b_direccion, id_contacto,telefono, correo):
+        #La clase esta heredando de contacto
+        Contacto.__init__(self,id_contacto,telefono, correo)
         self.b_nombre = b_nombre
         self.id_biblio = id_biblio
         self.direccion = b_direccion
