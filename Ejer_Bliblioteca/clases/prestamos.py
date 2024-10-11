c29394ebca83870c2f301a2cc28907a6fb80a1d9
diego gomez <diegog.v465@gmail.com>
2024-10-11 00:31:07 -0300

    coherencia con uml

--- Ejer_Bliblioteca/clases/prestamos.py
+++ Ejer_Bliblioteca/clases/prestamos.py
@@ -3,11 +3,11 @@ from usuario import Usuario
 from disponibilidad_libro import disponibilidadLibro
 from datetime import datetime #se importa la biblioteca datatime
 class Prestamo(Libro,Usuario,disponibilidadLibro):
-    def __init__(self, id_prestamo,fecha_prestamo,fecha_estimada, fecha_devolucion,dias_retraso,id_usuario , nombre,us_estado , isbn, titulo,cantidad_libro):
+    def __init__(self, id_prestamo,fecha_prestamo,fecha_estimada, fecha_devolucion,dias_retraso,id_usuario , nombre,us_estado , isbn, titulo,id_dispo ,cantidad_libro):
         #Clases padres (Usuario,Libro,disponibilidadLibro) y atributos heredados, se aplica herencia múltiple
         Usuario.__init__(self,id_usuario,nombre,us_estado)
         Libro.__init__(self,isbn, titulo)
-        disponibilidadLibro.__init__(self,cantidad_libro)
+        disponibilidadLibro.__init__(self,id_dispo,cantidad_libro)
         self.id_prestamo = id_prestamo #atributo adicional de la clase
         self.fecha_prestamo = datetime.now().date #se usa la funcion .date de la biblioteca datatime para extraer solo la fecha
         self.fecha_estimada = fecha_estimada
